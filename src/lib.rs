a8c834a85c52a414d90d46c7ceccd5efbead99fc
Gabriel Lacroix <lacroixgabriel@gmail.com>
2023-03-13 14:27:15 -0400

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use std::hash::Hash;
+
 use identity_hash::IdentityHashMap;
-use lexer::lex;
+use lexer::{lex, Token};
 use rustc_hash::FxHashSet as HashSet;
 
 pub mod fingerprint;
@@ -17,6 +19,71 @@ pub fn detect_plagiarism<S: AsRef<str>>(
     should_lex: bool,
     documents: &[S],
 ) -> Vec<(usize, usize)> {
+    if should_lex {
+        let lexed_documents = documents
+            .iter()
+            .map(|document| lex(document.as_ref()))
+            .collect::<Vec<_>>();
+
+        // First pass does no transformation on the original documents
+        let lexed_documents_no_transformation = lexed_documents.iter();
+
+        // Second pass ignores whitespace and comments
+        let lexed_documents_no_whitespace_no_comments = lexed_documents.iter().map(|document| {
+            document
+                .iter()
+                .filter(|&token| token != &Token::Whitespace && matches!(token, Token::Comment(_)))
+        });
+
+        // Third pass additionally ignores register names
+        let lexed_documents_no_register_names = lexed_documents_no_whitespace_no_comments
+            .clone()
+            .map(|document| {
+                document.map(|token| match token {
+                    Token::Register(_) => &Token::Register(0),
+                    _ => token,
+                })
+            });
+
+        // Return matches from all passes
+        // TODO: Change noise and guarantee thresholds to be per-pass
+        [
+            detect_plagiarism_generic(
+                noise_threshold,
+                guarantee_threshold,
+                lexed_documents_no_transformation,
+            ),
+            detect_plagiarism_generic(
+                noise_threshold,
+                guarantee_threshold,
+                lexed_documents_no_whitespace_no_comments,
+            ),
+            detect_plagiarism_generic(
+                noise_threshold,
+                guarantee_threshold,
+                lexed_documents_no_register_names,
+            ),
+        ]
+        .concat()
+    } else {
+        let document_bytes = documents
+            .iter()
+            .map(|document| document.as_ref().as_bytes());
+
+        detect_plagiarism_generic(noise_threshold, guarantee_threshold, document_bytes)
+    }
+}
+
+fn detect_plagiarism_generic<Docs, Doc, Token>(
+    noise_threshold: usize,
+    guarantee_threshold: usize,
+    documents: Docs,
+) -> Vec<(usize, usize)>
+where
+    Docs: IntoIterator<Item = Doc>,
+    Doc: IntoIterator<Item = Token>,
+    Token: Hash,
+{
     // Maps a hash to the index of the document in which it was first seen
     // To prevent rehashing the hashes, we use a hash map which does not rehash the keys.
     let mut hashes_seen: IdentityHashMap<usize> = IdentityHashMap::default();
@@ -24,15 +91,9 @@ pub fn detect_plagiarism<S: AsRef<str>>(
     // Keep matches in a hash set so that matches are not reported multiple times.
     let mut matches: HashSet<(usize, usize)> = HashSet::default();
 
-    for (index, document) in documents.iter().enumerate() {
-        let fingerprint = if should_lex {
-            let tokens = lex(document.as_ref());
-            fingerprint::fingerprint(noise_threshold, guarantee_threshold, &tokens)
-        } else {
-            // Use bytes instead of chars since it shouldn't affect the result and is faster.
-            let characters = document.as_ref().as_bytes();
-            fingerprint::fingerprint(noise_threshold, guarantee_threshold, characters)
-        };
+    for (index, document) in documents.into_iter().enumerate() {
+        let tokens = document.into_iter().collect::<Vec<Token>>();
+        let fingerprint = fingerprint::fingerprint(noise_threshold, guarantee_threshold, &tokens);
 
         for hash in fingerprint.hashes {
             match hashes_seen.get(&hash) {
